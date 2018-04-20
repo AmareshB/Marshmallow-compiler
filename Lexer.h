a9d84fa1cac2255195383dd2d01084de1cae1f30
pvenugo <32775579+pvenugo@users.noreply.github.com>
2018-04-19 22:26:16 -0700

    Lexer
    Added lexer -Sprint 1

---
+++ Lexer.h
@@ -0,0 +1,35 @@
+//
+// Created by prasanth on 19/4/18.
+//
+
+#ifndef SER502_SPRING2018_TEAM15_LEXER_H
+#define SER502_SPRING2018_TEAM15_LEXER_H
+
+#include <iostream>
+#include <map>
+#include <utility>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <array>
+#include <ostream>
+#include <ctype.h>
+
+class Lexer{
+    std::vector<char> input;
+    std::vector<std::string> tokens;
+    std::map<std::string, std::string> Keyword;
+    std::map<std::string, std::string>::iterator it;
+public:
+    Lexer(std::vector<char> program);
+    ~Lexer();
+    std::vector<std::string> Tokenize();
+    bool isDigit(char c);
+    bool isAlpha(char c);
+    bool isNewLine(char c);
+    int leadingSpaces(int i);
+    int getNumber(int i);
+
+};
+
+#endif //SER502_SPRING2018_TEAM15_LEXER_H
